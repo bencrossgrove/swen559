d17af993e56de0d8aaf768635b5c3593082ce0b4
Abigail Tran <mt2915@rit.edu>
2020-02-10 13:35:13 -0500

    Malachowsky photo 2

--- test-app/src/firebase.js
+++ test-app/src/firebase.js
@@ -1,14 +1,14 @@
-import firebase from 'firebase';
+import firebase from 'firebase'
 const config = {
-    apiKey: "AIzaSyAj1l42bYZUAJmxtiJv6WUoSSNLyWwrLp4",
-    authDomain: "swen-559.firebaseapp.com",
-    databaseURL: "https://swen-559.firebaseio.com",
-    projectId: "swen-559",
-    storageBucket: "swen-559.appspot.com",
-    messagingSenderId: "1018085688552",
-    appId: "1:1018085688552:web:ea001ae3a6376a4621755d",
-    measurementId: "G-0EVZ7DDPK3"
-  };
+  apiKey: 'AIzaSyAj1l42bYZUAJmxtiJv6WUoSSNLyWwrLp4',
+  authDomain: 'swen-559.firebaseapp.com',
+  databaseURL: 'https://swen-559.firebaseio.com',
+  projectId: 'swen-559',
+  storageBucket: 'swen-559.appspot.com',
+  messagingSenderId: '1018085688552',
+  appId: '1:1018085688552:web:ea001ae3a6376a4621755d',
+  measurementId: 'G-0EVZ7DDPK3'
+}
 
-  firebase.initializeApp(config);
-  export default firebase;
+firebase.initializeApp(config)
+export default firebase
